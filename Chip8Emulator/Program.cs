961cac1ba8c0b7108b3fd83900d51388b8348b16
Zoran Bosnjak <zoran0406@hotmail.com>
2025-05-26 22:43:35 +0200

    Initialize empty project

---
+++ Chip8Emulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Chip8Emulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
